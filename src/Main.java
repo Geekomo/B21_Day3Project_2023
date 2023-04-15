75f47028adccbed11002c318b96b7ac907c8d9d1
Geekomo <meerim210@gmail.com>
2023-04-15 15:08:20 +0200

    working on nice message 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git last day off Git learning!");
         //write your code here
+        //B21 is excited!!!
     }
 }
